676f8825d9cd288958a6efaddd3a846212e57c94
Chirag Lad <chiragladn92@gmail.com>
2016-09-11 23:18:42 +0530

    Initial UI created

---
+++ AutoParts.MainApplication/Views/AutoPartsOverviewView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace AutoParts.MainApplication.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AutoPartsOverviewView : Window
+    {
+        public AutoPartsOverviewView()
+        {
+            InitializeComponent();
+        }
+
+        private void EditButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var autoPartsDetailView = new AutoPartsDetailView();
+            autoPartsDetailView.ShowDialog();
+        }
+    }
+}
