f13a6a2332ae6634a5fc14e4d4b756d1d0bb6dae
Chirag Lad <chiragladn92@gmail.com>
2016-09-28 03:22:33 +0530

    Unit Test support added 2

---
+++ AutoParts.Test/AutoParts.MainApplication.Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoParts.MainApplication.Services;
+using AutoParts.Model;
+using Moq;
+using NUnit.Framework;
+
+namespace AutoParts.Test
+{
+    [TestFixture]
+    public class AutoParts
+    {
+        [OneTimeSetUp]
+        public void CreateDependencies()
+        {
+            
+        }
+
+        [Test]
+        public void ShouldLoadAllCoffees()
+        {
+            //Arrange
+            ObservableCollection<Parts> parts = null;
+            var mock = new Mock<IDataAccessService>(MockBehavior.Loose);
+            var expectedParts = coffeeDataService.GetAllCoffees();
+
+            //act
+            var viewModel = GetViewModel();
+            coffees = viewModel.Coffees;
+
+            //assert
+            CollectionAssert.AreEqual(expectedCoffees, coffees);
+        }
+    }
+}
